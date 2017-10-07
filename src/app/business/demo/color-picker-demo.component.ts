d340fae6ff748973c79ffba473b8993369bdbbed
332557712@qq.com <332557712@qq.com>
2017-10-07 14:18:16 +0800

    修改表单按钮级样式与颜色选择器示例

--- src/app/business/demo/color-picker-demo.component.ts
+++ src/app/business/demo/color-picker-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { AppService } from '../../app.service';
 
@@ -11,22 +11,24 @@ import { AppService } from '../../app.service';
 })
 export class ColorPickerDemoComponent {
 
-  color:string='#ff0000';
+  color: string = '#ff0000';
 
   color2: any = {
     r: 100, g: 130, b: 150
-};
+  };
 
-color3: any = {
+  color3: any = {
     h: 100, s: 50, b: 50
-};
- 
+  };
+
+  selectColor:string='#1190d6';
+
   constructor(private appService: AppService) {
     this.appService.titleEventEmitter.emit("颜色选择器示例");
   }
 
 
-  
+
 
 
 
